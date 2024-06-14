ce416f720f90b41e7a9a77acf103dad33027da53
Stephane Hamel <koydas@hotmail.com>
2024-06-14 12:39:33 -0400

    use of createRoot

--- apps/simple-react/src/app.js
+++ apps/simple-react/src/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import LanguagePicker from '../components/language-picker/language-picker.js'
 
 function App() {
   return (
     <div>
-      <h1>Intro</h1>
+      
+      <h1>TODO</h1>
+      
     </div>
   );
 }
