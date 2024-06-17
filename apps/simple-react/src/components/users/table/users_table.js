ae8e995f64d254c50dbd98ce98c4a612ad2cd183
Stephane Hamel <koydas@hotmail.com>
2024-06-16 23:20:47 -0400

    use of icon inside button

--- apps/simple-react/src/components/users/table/users_table.js
+++ apps/simple-react/src/components/users/table/users_table.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import styled from "styled-components"
 import { ButtonLight as Button } from "../../forms/buttons.js"
 import { DataCell, HeaderCell, Row, TBody, THead, Table } from "../../table/table.js"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
-
-const Icon = styled(FontAwesomeIcon)`font-size: smaller;`
 
 const UsersTable = ({ data, remove }) => <Table>
     <THead>
@@ -26,15 +22,12 @@ const UsersTable = ({ data, remove }) => <Table>
                     <DataCell>{first_name}</DataCell>
                     <DataCell>{last_name}</DataCell>
                     <DataCell>
-                        <Button onClick={remove}>
-                            <Icon icon={faTrashCan} />
-                        </Button>
+                        <Button onClick={remove} icon={faTrashCan} />
                     </DataCell>
                 </Row>
             )
         }
     </TBody>
-
 </Table>
 
 export default UsersTable        
