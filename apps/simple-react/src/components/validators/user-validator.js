27608e2b9488c48ef1b93216cde5b54376086144
Stephane Hamel <koydas@hotmail.com>
2024-06-16 20:55:02 -0400

    tests user-validator

--- apps/simple-react/src/components/validators/user-validator.js
+++ apps/simple-react/src/components/validators/user-validator.js
@@ -1,18 +1,15 @@
 export function validate_user({ first_name_input, last_name_input }) {
-    let valid = true
+    const first_name_valid = validate_input(first_name_input)
+    const last_name_valid = validate_input(last_name_input)
 
-    valid &&= validate_input(first_name_input)
-    valid &&= validate_input(last_name_input)
-
-    return valid
+    return first_name_valid && last_name_valid
 }
 
 function validate_input(input) {
     
     if (!input.value) {
         input.classList?.add('invalid')
-console.log('nope')
-console.log(input.value)
+
         return false
     }
 
--- apps/simple-react/src/components/validators/user-validator.test.js
+++ apps/simple-react/src/components/validators/user-validator.test.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import '@testing-library/dom'
+import { JSDOM } from 'jsdom'
 import { validate_user } from "./user-validator";
 
 describe('validate_user', () => {
+    const first_name_input = new JSDOM(<input />)
+    const last_name_input = new JSDOM(<input />)
+
+    beforeEach(() => {
+        first_name_input.value = ""
+        last_name_input.value = ""
+    })
+
     it('no values - both', () => {
-        const first_name_input = () => document.createElement(<input value='ok'/>)
-        const last_name_input = () => document.createElement(<input />)
+        const result = validate_user({ first_name_input, last_name_input })
+
+        expect(result).toBe(false)
+        
+    })
+
+    it('with values - only first name', () => {
+        first_name_input.value = "Bob"
+        const result = validate_user({ first_name_input, last_name_input })
+
+        expect(result).toBe(false)
+        
+    })
+
+    it('with values - only last name', () => {
+        last_name_input.value = "Gratton"
 
         const result = validate_user({ first_name_input, last_name_input })
 
@@ -13,33 +35,13 @@ describe('validate_user', () => {
         
     })
 
-    // it('no values - first name', () => {
-    //     const first_name_input = <input />
-    //     const last_name_input = <input value='Gratton' />
-
-    //     const result = validate_user({ first_name_input, last_name_input })
-
-    //     expect(result).toBe(false)
+    it('with values - both', () => {
+        first_name_input.value = "Bob"
+        last_name_input.value = "Gratton"
         
-    // })
+        const result = validate_user({ first_name_input, last_name_input })
 
-    // it('no values - last name', () => {
-    //     const first_name_input = <input value="Bob" />
-    //     const last_name_input = <input />
-
-    //     const result = validate_user({ first_name_input, last_name_input })
-
-    //     expect(result).toBe(false)
+        expect(result).toBe(true)
         
-    // })
-
-    // it('with values - both', () => {
-    //     const first_name_input = <input value='Bob' />
-    //     const last_name_input = <input value='Gratton' />
-
-    //     const result = validate_user({ first_name_input, last_name_input })
-
-    //     expect(result).toBe(true)
-        
-    // })
+    })
 })
