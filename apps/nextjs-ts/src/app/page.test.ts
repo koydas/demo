cc09e80874070db1683629f13b10e5cfcaae5bb8
Stephane Hamel <koydas@hotmail.com>
2024-06-20 14:23:08 -0400

    home page tests

--- apps/nextjs-ts/src/app/page.test.ts
+++ apps/nextjs-ts/src/app/page.test.ts
@@ -1,11 +1,26 @@
-
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page, { LinkDesktop, LinkMobile } from './page'
 
 describe('Links', () =>{
     it('Mobile', () => {
-        // expect(LinkDesktop).toBe(null)
+        expect(LinkMobile).toBeDefined()
+        expect(typeof LinkMobile).toBe('function')
+        
+        const props = LinkMobile().props
+        
+        expect(props.href).toBe('/mobile')
+        expect(props.children).toBe('Mobile')
     })
 
     it('Desktop', () => {
-        // expect(LinkDesktop).toBe(null)
+        expect(LinkDesktop).toBeDefined()
+        expect(typeof LinkDesktop).toBe('function')
+        
+        const props = LinkDesktop().props
+        
+        expect(props.href).toBe('/desktop')
+        expect(props.children).toBe('Desktop')
+
     })
 })
--- apps/nextjs-ts/src/app/page.tsx
+++ apps/nextjs-ts/src/app/page.tsx
@@ -1,39 +1,11 @@
+import Link from 'next/link'
+
+export const LinkMobile = () => <Link href='/mobile'>Mobile</Link>
+export const LinkDesktop = () => <a href='/desktop'>Desktop</a>
 
 export default function Home() {
-  const [data, setData] = useState(users)
-
-  function add() {
-    if (!validate_form()) return
-
-    const user = get_user(users)
-    users.push(user)
-
-    setData([...users])
-
-    resetForm()
-  }
-
-  function remove(e) {
-    const row = e.target.closest('tr')
-    const id = row.attributes['data-id'].value
-
-    if (!id)
-      return
-
-    users = users.filter(x => x.id != id)
-
-    setData([...users.filter(x => x.id != id)])
-  }
-
-  return (
-    <AppWrapper>
-      <TopButtons>
-        <UserForm add={add}/>
-      </TopButtons>
-
-      <Content>
-        <UsersTable data={data} remove={remove} />
-      </Content>
-    </AppWrapper>
-  );
+  return <>
+    <LinkMobile />
+    <LinkDesktop />
+  </>
 }
