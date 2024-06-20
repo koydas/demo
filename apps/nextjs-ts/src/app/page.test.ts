4bf20787f2a97a9b51cfba77ecff3e50d60f1fd4
Stephane Hamel <koydas@hotmail.com>
2024-06-20 13:39:21 -0400

    tests file added

---
+++ apps/nextjs-ts/src/app/page.test.ts
@@ -0,0 +1,11 @@
+
+
+describe('Links', () =>{
+    it('Mobile', () => {
+        // expect(LinkDesktop).toBe(null)
+    })
+
+    it('Desktop', () => {
+        // expect(LinkDesktop).toBe(null)
+    })
+})
